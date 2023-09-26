7e8ae6cf6b31d5d502350d7c99a7deef16ab86e6
jamyspencer <jamy_spencer@hotmail.com>
2023-09-26 07:44:56 -0500

    added dev proxy for api calls

--- src/stores/map.ts
+++ src/stores/map.ts
@@ -1,19 +1,32 @@
 import { defineStore } from 'pinia'
-import { map, latLng, tileLayer, MapOptions, LatLngExpression } from 'leaflet'
+import { map, geoJSON, tileLayer, MapOptions, LatLngExpression, Map } from 'leaflet'
+
+type OptionalMap = Map | null
 
 export const useMapStore = defineStore('map', {
-    state: () => ({ map: null }),
+    state: () => ({ map: null as OptionalMap }),
     actions:{
       init() {
         const options: MapOptions = {
-            center: { lat: 40.731253, lng:-73.996139 },
+            center: { lat: 50.4501, lng:30.5234 },
             zoom: 12,
         };
-        this.map = map('map').setView([51.505, -0.09], 13)
+        this.map = map('map', options)
         tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
+        this.getRailInfo()
+      },
+      getRailInfo(){
+        if (this.map != null) {
+          const mapRef = this.map
+          fetch("/api/railroads/")
+            .then(response => response.json().then(data => {
+              const gjson = geoJSON(data)
+              mapRef.addLayer(gjson)
+            }))
+          }
       },
       focus(latlng: LatLngExpression, zoom: Number){
         this.map.setView(latlng, zoom);
