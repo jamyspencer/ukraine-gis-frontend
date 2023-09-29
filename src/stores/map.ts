385505c28122eba1e5ca13e20a8aa1155816b130
jamyspencer <jamy_spencer@hotmail.com>
2023-09-29 11:55:36 -0500

    figured out how to translate between projection types

--- src/stores/map.ts
+++ src/stores/map.ts
@@ -1,12 +1,16 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-import { Map, View } from 'ol'
-import TileLayer from 'ol/layer/Tile'
-import { OSM } from 'ol/source'
-import {  fromLonLat } from 'ol/proj'
+import { Feature, Map, View } from 'ol'
+import { OSM, Vector as VectorSource } from 'ol/source'
+import {  fromLonLat, toLonLat } from 'ol/proj'
 import Layer from 'ol/layer/Layer'
-import BaseLayer from 'ol/layer/Base'
-import LayerGroup from 'ol/layer/Group'
+import {Tile as TileLayer} from 'ol/layer';
+import VectorLayer  from 'ol/layer/Vector'
+import GeoJSON from 'ol/format/GeoJSON'
+import Stroke from 'ol/style/Stroke'
+import Style from 'ol/style/Style'
+import { Circle, LineString, MultiLineString } from 'ol/geom'
+import {useGeographic} from 'ol/proj.js';
 type OptionalMap =  Map | null
 
 const mapGuard = (obj: any): obj is Map => {
@@ -15,40 +19,48 @@ const mapGuard = (obj: any): obj is Map => {
 
 export const useMapStore = defineStore('map', ()=>{
   const map = ref<OptionalMap>(null)
-  const group = new LayerGroup({ layers:[new TileLayer( {source: new OSM()})]} )
+  const osm = new TileLayer( {source: new OSM()})
   const overlayLayers = ref<Array<Layer>>([])
 
-  async function init() {
-    map.value = new Map({
-      target: 'map-container',
-      layers: group,
-      view: new View({
-        center: fromLonLat([32,48]),
-        zoom: 6,
-      }),
-    });
-  }
-/*
-  async function getRailInfo(m: Map){
+  async function init(reload = false) {
+
+    const rails =  await getRailInfo()
+    const group = rails ? [osm,rails] : [osm]
+    if (map.value == null || reload) {
+      const m = new Map({
+        target: 'map-container',
+        layers: group,
+        view: new View({
+          center: fromLonLat([32,48]),
+          zoom: 6,
+        }),
+      });
+      map.value = m
+    }
+}
+
+  async function getRailInfo(){
     const response = await fetch("/api/railroads/")
 
     if (response.ok) {
-      const data = await response.json()
-      const gjson = geoJSON(data, {
-        onEachFeature: (feature, layer) => layer.bindPopup(feature.properties.exs_descri),
-        style: feature => {
-          if (feature != null && feature.properties.exs_descri != null ) {
-            if ( "Operational" != feature.properties.exs_descri ) {
-              return {color: "#d9534f"}
-            } 
-          }
-          return {color: "#428bca"}
-        }
+      let data = await response.json()
+      const source = new VectorSource({features: (new GeoJSON()).readFeatures(data, {
+        dataProjection: 'EPSG:4326',
+        featureProjection: 'EPSG:3857'
+      })})
+
+      return new VectorLayer({
+        source,
+        style: new Style({
+          stroke: new Stroke({
+            color: 'blue',
+            width: 2
+          })
+        })
       })
-      overlays.value["railroads"] = gjson
-      m.addLayer(gjson)
     }
   }
+  /*
 
   function manageControlLayers(m: Map){
     const c = control.layers(baselayers.value, overlays.value)
